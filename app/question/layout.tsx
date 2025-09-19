f906c88b3114ddc2e12adef482a53f1ea80c06ca
Tina <a03993a@gmail.com>
2025-09-19 16:34:15 +0800

    feat: add side panel component

---
+++ app/question/layout.tsx
@@ -0,0 +1,19 @@
+import SidePanel from "@/components/side-panel";
+import UserMenu from "@/components/user-menu";
+
+interface LayoutProps {
+    children: React.ReactNode;
+}
+
+export default function QuestionLayout(props: LayoutProps) {
+    return (
+        <div className="flex h-full">
+            <div className="flex h-full w-[260px] flex-col rounded-2xl bg-white shadow-lg">
+                <SidePanel />
+                <UserMenu />
+            </div>
+
+            <div className="flex flex-1">{props.children}</div>
+        </div>
+    );
+}
--- app/question/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function QuestionPage() {
-    return <h1>Question Page</h1>;
-}
