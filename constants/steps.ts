f7acd14ee0bacf8a95792a7d318f7e9833bb892d
Tina <a03993a@gmail.com>
2025-10-01 16:43:15 +0800

    feat: add experience new page

--- constants/steps.ts
+++ constants/steps.ts
@@ -2,24 +2,25 @@ export const STEPS = [
     {
         id: 1,
         title: "基本資訊",
-        description:
-            "填寫你的面試經驗，並且可以查看已分享的心得\n幫助你快速整理個人資料！",
+        path: "/experience/new",
+        description: "填寫你的面試經驗，並且可以查看已分享的心得\n幫助你快速整理個人資料！",
     },
     {
         id: 2,
         title: "面試流程",
-        description:
-            "把遇到的面試流程完整填寫，越詳細越方便回顧\n面試考題請在下一步填寫！",
+        path: "/experience/new/step2",
+        description: "把遇到的面試流程完整填寫，越詳細越方便回顧\n面試考題請在下一步填寫！",
     },
     {
         id: 3,
         title: "面試考題",
-        description:
-            "輸入你遇到的面試題目！\n填寫越完整，有機會獲得更多提問次數，也方便未來回顧！",
+        path: "/experience/new/step3",
+        description: "輸入你遇到的面試題目！\n填寫越完整，有機會獲得更多提問次數，也方便未來回顧！",
     },
     {
         id: 4,
         title: "",
-        description:"",
+        path: "",
+        description: "",
     },
 ];
