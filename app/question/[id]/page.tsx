89a06f303b6647c402717a476404df4ca6a0ee1e
Tina <a03993a@gmail.com>
2025-10-03 13:52:51 +0800

    feat: add question detail page

--- app/question/[id]/page.tsx
+++ app/question/[id]/page.tsx
@@ -1,3 +1,119 @@
+"use client";
+
+import clsx from "clsx";
+import { useState, useRef, useMemo, useEffect } from "react";
+
+import SubmitIcon from "/components/icons/submit.svg";
+
+import { useParams } from "next/navigation";
+import { MOCK_CHATS, type Chats } from "@/constants/mock-chats";
+
+const MAX_HEIGHT = 160;
+
 export default function QuestionDetailPage() {
-    return <h1>Question Detail Page</h1>;
+    const params = useParams();
+    const recordId = params.id;
+
+    const [chats, setChats] = useState<Chats | null>();
+    const [value, setValue] = useState("");
+    const [textareaHeight, setTextareaHeight] = useState(0);
+
+    const textareaRef = useRef<HTMLTextAreaElement>(null);
+
+    useEffect(() => {
+        // TODO: call API
+        const chat = MOCK_CHATS.find((c) => c.id === recordId);
+
+        setChats(chat);
+    }, [recordId]);
+
+    const isSubmitDisabled = useMemo(() => {
+        return value.trim() == "";
+    }, [value]);
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+
+        // TODO: call API
+        // TODO: 儲存對話紀錄並生成回答
+
+        setValue("");
+    };
+
+    useEffect(() => {
+        if (textareaRef.current) {
+            textareaRef.current.style.height = "auto";
+            const updatedHeight = Math.min(textareaRef.current.scrollHeight, MAX_HEIGHT);
+            textareaRef.current.style.height = `${updatedHeight}px`;
+            setTextareaHeight(updatedHeight);
+        }
+    }, [value]);
+
+    if (!chats) {
+        return null;
+    }
+
+    return (
+        <div className="relative flex h-full flex-1 flex-col">
+            <div className="mb-20 overflow-y-auto">
+                <div className="py-10" style={{ paddingBottom: `${textareaHeight + 48}px` }}>
+                    <div className="mx-auto w-[704px]">
+                        <div className="flex flex-col gap-10 whitespace-pre-line text-gray-600">
+                            {chats.chat.message.map((item, index) => (
+                                <div key={index} className="flex flex-col gap-10">
+                                    <div className="ml-auto flex w-fit max-w-[480px] flex-col gap-2 rounded-3xl bg-gray-100 px-4 py-2">
+                                        {item.options.map((item, index) => {
+                                            return (
+                                                <span
+                                                    key={index}
+                                                    className="w-fit rounded-full bg-teal-100 px-2 py-1 text-sm text-teal-600"
+                                                >
+                                                    {item}
+                                                </span>
+                                            );
+                                        })}
+
+                                        {item.content && <div>{item.content}</div>}
+                                    </div>
+
+                                    <div className="flex flex-col gap-3 text-gray-600">
+                                        <div>{item.response}</div>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="absolute bottom-0 w-full pb-6">
+                <div className="relative mx-auto flex w-[704px] flex-col rounded-3xl border border-gray-400 bg-white p-3 focus-within:border-teal-500">
+                    <textarea
+                        ref={textareaRef}
+                        placeholder="詢問任何面試相關問題"
+                        value={value}
+                        onChange={(e) => setValue(e.target.value)}
+                        className="w-full resize-none bg-transparent pr-10 focus:outline-none"
+                        rows={1}
+                    />
+
+                    <button
+                        type="submit"
+                        disabled={isSubmitDisabled}
+                        onClick={handleSubmit}
+                        className="absolute bottom-2 right-3 disabled:pointer-events-none"
+                    >
+                        <SubmitIcon
+                            className={clsx("h-8 w-8 rounded-full p-1", {
+                                "pointer-events-none bg-gray-100 text-gray-200": isSubmitDisabled,
+                                "cursor-pointer bg-teal-950 text-gray-50 hover:bg-teal-400 active:bg-teal-500": !isSubmitDisabled,
+                            })}
+                        />
+                    </button>
+                </div>
+
+                <p className="pt-2 text-center text-sm text-gray-400">ChatJOB 可能會出錯。請查核重要資訊。</p>
+            </div>
+        </div>
+    );
 }
